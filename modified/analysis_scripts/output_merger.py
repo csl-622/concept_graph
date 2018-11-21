43f67a3dc62a35fecbe433e3eaae9bf1ff01bc35
Pratik Chhajer <2015csb1025@iitrpr.ac.in>
2018-11-21 19:14:55 +0530

    Update output_merger.py
    Id2Id blunder fixed!!

--- modified/analysis_scripts/output_merger.py
+++ modified/analysis_scripts/output_merger.py
@@ -2,58 +2,59 @@ import os
 import pickle
 
 Dictionary = {}
-with open("output/1_dict_merged.txt", "r") as file:
-    for line in file:
-        L = line[:-1]
-        L = L.split('\t')
-        if len(L) == 2:
-            Dictionary[L[0]] = int(L[1])
-            
-Edges = {}
-with open("output/1_graph.txt", "r") as file:
-    for line in file:
-        L = line[:-1]
-        L = L.split('\t')
-        if len(L) == 3:
-            Edges[(int(L[0]),int(L[1]))] = int(L[2])  
-
-
-for i in range(2,25):
-    
-    print(i)
-    
-    Id2Id = {}
-    with open("output/" + str(i) + "_dict_merged.txt", "r") as file:
+with open("output/13_dict_merged.txt", "r") as file:
         for line in file:
-            L = line[:-1]
-            L = L.split('\t')
-            if len(L) == 2:
-                if L[0] in Dictionary:
-                    Id2Id[int(L[1])] = Dictionary[L[0]]
-                else:
-                    Id2Id[int(L[1])] = len(Dictionary)
-    
-    print('Dictionary done!')
+                L = line[:-1]
+                L = L.split('\t')
+                if len(L) == 2:
+                        Dictionary[L[0]] = int(L[1])
 
-    with open("output/" + str(i) + "_graph.txt", "r") as file:
-    for line in file:
-        L = line[:-1]
-        L = L.split('\t')
-        if len(L) == 3:
-            a = int(L[0])
-            b = int(L[1])
-            c = int(L[2])
-            if (a,b) in Edges:
-                Edges[(a,b)] += c
-            else:
-                Edges[(a,b)] = c
+Edges = {}
+with open("output/13_graph.txt", "r") as file:
+        for line in file:
+                L = line[:-1]
+                L = L.split('\t')
+                if len(L) == 3:
+                        Edges[(int(L[0]),int(L[1]))] = int(L[2])
 
-    print('Graph done!')
+print('Reading Input done')
 
-    with open('dict' + str(i) + '.txt', 'wb') as handle:
-        pickle.dump(Dictionary,handle)
+for i in range(2,13):
+        
+        print(i)
+        
+        Id2Id = {}
+        with open("output/" + str(i) + "_dict_merged.txt", "r") as file:
+                for line in file:
+                        L = line[:-1]
+                        L = L.split('\t')
+                        if len(L) == 2:
+                                if L[0] in Dictionary:
+                                        Id2Id[int(L[1])] = Dictionary[L[0]]
+                                else:
+                                        Id2Id[int(L[1])] = len(Dictionary)
+        
+        print('Dictionary done!')
 
-    with open('graph' + str(i) + '.txt', 'wb') as handle:
-        pickle.dump(Edges,handle)
+        with open("output/" + str(i) + "_graph.txt", "r") as file:
+                for line in file:
+                        L = line[:-1]
+                        L = L.split('\t')
+                        if len(L) == 3:
+                                a = Id2Id[int(L[0])]
+                                b = Id2Id[int(L[1])]
+                                c = int(L[2])
+                                if (a,b) in Edges:
+                                        Edges[(a,b)] += c
+                                else:
+                                        Edges[(a,b)] = c
 
-    print('Dumping done!')
+        print('Graph done!')
+
+        with open('dict' + str(i) + '.pickle', 'wb') as handle:
+                pickle.dump(Dictionary,handle)
+
+        with open('graph' + str(i) + '.pickle', 'wb') as handle:
+                pickle.dump(Edges,handle)
+
+        print('Dumping done!')
