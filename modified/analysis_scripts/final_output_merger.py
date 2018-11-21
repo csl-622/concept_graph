c596f8c78c805f04e74b789cd27249ca0ebc7f71
Pratik Chhajer <2015csb1025@iitrpr.ac.in>
2018-11-21 19:46:27 +0530

    Create final_output_merger.py
    To finally merge output of two final merger output of 1 to 12 and 13 to 24

---
+++ modified/analysis_scripts/final_output_merger.py
@@ -0,0 +1,54 @@
+import os
+import pickle
+
+with open('dict12.txt','rb') as handle:
+	Dictionary = pickle.load(handle)
+
+print('Dictionary reading done!')
+
+with open('graph12.txt','rb') as handle:
+	Edges = pickle.load(handle)
+
+print('Edges reading done!')
+
+with open('dict24.txt','rb') as handle:
+	Dictionary_ = pickle.load(handle)
+
+print('Dictionary_ reading done!')
+
+with open('graph24.txt','rb') as handle:
+	Edges_ = pickle.load(handle)
+
+print('Edges_ reading done!')
+
+Id2Id = {}
+
+for key, val in Dictionary_.items():
+	if key in Dictionary:
+		Id2Id[val] = Dictionary[key]
+	else:
+		Id2Id[val] = len(Dictionary)
+
+print('Dictionary done!')
+
+for key, val in Edges_.items():
+	a = Id2Id[key[0]]
+	b = Id2Id[key[1]]
+	if (a,b) in Edges:
+		Edges[(a,b)] += val
+	else:
+		Edges[(a,b)] = val
+
+print('Graph done!')
+
+with open('dict.pickle','wb') as handle:
+	pickle.dump(Dictionary,handle)
+
+print('Dictionary dumped!')
+
+with open('graph.pickle','wb') as handle:
+	pickle.dump(Edges,handle)
+
+print('Graph dumped!')
+
+print('Dumping done!')
