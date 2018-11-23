ac426553cc151e4df11a1db5236e6a551768a484
Pratik Chhajer <2015csb1025@iitrpr.ac.in>
2018-11-23 23:34:23 +0530

    Plots added

--- modified/analysis_scripts/visualize_concept_graph_different_threshold.py
+++ modified/analysis_scripts/visualize_concept_graph_different_threshold.py
@@ -1,24 +1,35 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 import os
+def fun(s):
+    ans = 0
+    S = list(s)
+    for i in S:
+        if ord(i) - ord('0') >= 0 and ord(i) - ord('9') <= 0:
+            ans *= 10
+            ans += int(i)
+    return ans
 
-A = sorted(os.listdir('pickles'))
-N = []
+L = sorted(os.listdir('pickles'))
+A = []
+for i in L:
+    A.append((fun(i),i))
+
+A = sorted(A)    
+   
 X = []
-Y = []
-Z = []
-c = 0
+Nodes = []
+Edges = []
 for i in A:
-    print(i)
-    c += 1
-    G = nx.read_gpickle(os.path.join('pickles',i))
+    G = nx.read_gpickle(os.path.join('pickles',i[1]))
     n = G.number_of_nodes()
     m = G.number_of_edges()
-    N.append(i)
-    X.append(c)
-    Y.append(n)
-    Z.append(m)
+    Nodes.append(n)
+    Edges.append(m)
+    X.append(i[0])
 
-print(N)
-print(X)
-print(Y)
-print(Z)
+plt.plot(X,Y,'r^')
+plt.show()
+
+plt.plot(X,Z,'g^')
+plt.show()
