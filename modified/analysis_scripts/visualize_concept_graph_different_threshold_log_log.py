cd797cf2f5c2c329259953bff1d39fd3ba4f8088
Pratik Chhajer <2015csb1025@iitrpr.ac.in>
2018-11-24 23:50:17 +0530

    Create visualize_concept_graph_different_threshold_log_log.py
    To get log log graph

---
+++ modified/analysis_scripts/visualize_concept_graph_different_threshold_log_log.py
@@ -0,0 +1,36 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import math
+import os
+def fun(s):
+    ans = 0
+    S = list(s)
+    for i in S:
+        if ord(i) - ord('0') >= 0 and ord(i) - ord('9') <= 0:
+            ans *= 10
+            ans += int(i)
+    return ans
+
+L = sorted(os.listdir('pickles'))
+A = []
+for i in L:
+    A.append((fun(i),i))
+
+A = sorted(A)    
+   
+X = []
+Nodes = []
+Edges = []
+for i in A:
+    G = nx.read_gpickle(os.path.join('pickles',i[1]))
+    n = G.number_of_nodes()
+    m = G.number_of_edges()
+    Nodes.append(math.log(n))
+    Edges.append(math.log(m))
+    X.append(i[0])
+
+plt.plot(X,Y,'r^')
+plt.show()
+
+plt.plot(X,Z,'g^')
+plt.show()
